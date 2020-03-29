260b073fc0ba4d3de80d937b0e9fba38a1ee07bf
Chris Ashman <chrisashman96@gmail.com>
2020-03-29 00:10:20 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_235220) do
+ActiveRecord::Schema.define(version: 2020_03_29_000942) do
 
 end
